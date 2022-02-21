dceb5a33c0d79bbe25084c2794412808a26ebf6b
saitej <saitej.galipelli@gmail.com>
2022-02-21 10:03:12 +0530

    src and mvn wrapper added

---
+++ src/main/java/guru/springframework/spring5appjokes/Spring5AppJokesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5appjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5AppJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5AppJokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring5appjokes/Spring5AppJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5appjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5AppJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
